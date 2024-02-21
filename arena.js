3c735a7742fb7d819623f9cd85f21ca784c686f1
hannahmariamoran <143641022+hannahmariamoran@users.noreply.github.com>
2024-02-20 19:37:53 -0500

    working on adding other content types back in to grid

--- arena.js
+++ arena.js
@@ -86,95 +86,95 @@ let renderBlock = (block) => {
 	// 	channelBlocks.insertAdjacentHTML('beforeend', textItem)
 	// }
 
-	// // Uploaded (not linked) media…
-	// else if (block.class == 'Attachment') {
-	// 	let attachment = block.attachment.content_type // Save us some repetition
+	// Uploaded (not linked) media…
+	else if (block.class == 'Attachment') {
+		let attachment = block.attachment.content_type // Save us some repetition
 
-	// 	// Uploaded videos!
-	// 	if (attachment.includes('video')) {
-	// 		// …still up to you, but we’ll give you the `video` element:
-	// 		let videoItem =
-	// 			`
-	// 			<li class="block block--video">
-	// 				<video controls src="${ block.attachment.url }"></video>
-	// 			</li>
-	// 			<li>
-	// 				<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
-	// 			</li>
-	// 			`
-	// 		channelBlocks.insertAdjacentHTML('beforeend', videoItem)
-	// 		// More on video, like the `autoplay` attribute:
-	// 		// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
-	// 	}
+		// Uploaded videos!
+		if (attachment.includes('video')) {
+			// …still up to you, but we’ll give you the `video` element:
+			let videoItem =
+				`
+				<li class="block block--video">
+					<video controls src="${ block.attachment.url }"></video>
+				</li>
+				<li>
+					<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
+			// More on video, like the `autoplay` attribute:
+			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+		}
 
-	// 	// Uploaded PDFs!
-	// 	else if (attachment.includes('pdf')) {
+		// Uploaded PDFs!
+		else if (attachment.includes('pdf')) {
 			
-	// 		let pdfItem =
-	// 			`
-	// 				<li class="block block--pdf">
-	// 					<a href="${block.attachment.url}">
-	// 						<figure>
-	// 							<img src="${block.image.large.url}" alt="${block.title}">
-	// 						</figure>
-	// 					</a>
-	// 				</li>
-	// 				<li>
-	// 					<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
-	// 				</li>
-	// 			`
-	// 		channelBlocks.insertAdjacentHTML('beforeend', pdfItem);
-	// 	}
+			let pdfItem =
+				`
+					<li class="block block--pdf">
+					<div class="block">
+						<a href="${block.attachment.url}" class="block--pdf">
+							<figure>
+								<img src="${block.image.large.url}" alt="${block.title}">
+							</figure>
+						</a>
+						<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
+					</div>
+					</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', pdfItem);
+		}
 
-	// 	// Uploaded audio!
-	// 	else if (attachment.includes('audio')) {
-	// 		// …still up to you, but here’s an `audio` element:
-	// 		console.log(block);
-	// 		let audioItem = 
-	// 			`
-	// 			<li class="block block--audio">
-	// 				<audio controls src="${ block.attachment.url }"></audio>
-	// 			</li>
-	// 			<li>
-	// 				<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
-	// 			</li>
-	// 			`
-	// 		channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-	// 	}
-	// }
+		// Uploaded audio!
+		else if (attachment.includes('audio')) {
+			// …still up to you, but here’s an `audio` element:
+			console.log(block);
+			let audioItem = 
+				`
+				<li>
+					<div class="block">
+						<audio controls src="${ block.attachment.url }" class="block--audio"></audio>
+						<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
+					</div>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
+		}
+	}
 
-	// // Linked media…
-	// else if (block.class == 'Media') {
-	// 	let embed = block.embed.type
+	// Linked media…
+	else if (block.class == 'Media') {
+		let embed = block.embed.type
 
-	// 	// Linked video!
-	// 	if (embed.includes('video')) {
-	// 		let linkedVideoItem =
-	// 			`
-	// 			<li class="block block--videolinked">
-	// 				${ block.embed.html }
-	// 			</li>
-	// 			<li>
-	// 				<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
-	// 			</li>
-	// 			`
-	// 		channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
-	// 	}
+		// Linked video!
+		if (embed.includes('video')) {
+			let linkedVideoItem =
+				`
+				<li>
+					<div class="block">
+						<div class="block--videolinked">${ block.embed.html }</div>
+						<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
+					</div>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
+		}
 
-	// 	// Linked audio!
-	// 	else if (embed.includes('rich')) {
-	// 		let linkedAudioItem =
-	// 			`
-	// 			<li class="block block--audiolinked">
-	// 				${ block.embed.html }
-	// 			</li>
-	// 			<li>
-	// 				<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
-	// 			</li>
-	// 			`
-	// 		channelBlocks.insertAdjacentHTML('beforeend', linkedAudioItem)
-	// 	}
-	// }
+		// Linked audio!
+		else if (embed.includes('rich')) {
+			let linkedAudioItem =
+				`
+				<li>
+					<div class="block">
+					<div class="block--audiolinked">${ block.embed.html }</div>
+					<h3 class="block-curator">Curated by<br>${block.connected_by_username}</h3>
+					</div>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', linkedAudioItem)
+		}
+	}
 }
 
 
